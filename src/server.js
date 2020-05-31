1cd1377c1f2e3f08d823fb8eb1019e3be17d85f0
Marcel Marschner <marcel.marschner@outlook.com>
2020-05-31 23:25:13 +0200

    - added package axios - added package cheerio - added first scraping example

--- src/server.js
+++ src/server.js
@@ -1,10 +1,30 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 const fastify = require('fastify')({
     logger: true
 });
 
 fastify.get('/', async (request, reply) => {
-    reply.type('application/json').code(200)
-    return {hello: 'world'}
+    reply.type('application/json').code(200);
+
+    axios.get(
+        'https://www.nintendo.de/Nintendo-eShop/Charts/Nintendo-eShop-Charts-1396788.html'
+    ).then(rawResponse => {
+        const cssSelector = cheerio.load(rawResponse.data);
+
+        const response = [];
+        cssSelector('.page-list-group-item a').get().forEach(item => {
+            response.push({
+                title: item.attribs['title']
+            });
+        });
+
+        reply.send(response);
+    })
+    .catch(() => {
+        reply.send([]);
+    });
 });
 
 fastify.listen(3000, (err, address) => {
