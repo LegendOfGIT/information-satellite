ffef370a81fedd7dc43a3840771e41582f879f51
Marcel Marschner <marcel.marschner@outlook.com>
2023-01-25 00:02:32 +0100

    - satellite now sends User-Agent with visits

--- src/scraping/commands/visit-uri.js
+++ src/scraping/commands/visit-uri.js
@@ -25,8 +25,8 @@ module.exports = (context = {}, parameters = {}) => new Promise(resolve => {
 
     const options = {
         "headers": {
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
-            "Accept-Language": 'de-DE, de;q=0.5'
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+            "Accept-Language": 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7'
         }
     };
     return httpClient.get(commandParameters.uri, options)
