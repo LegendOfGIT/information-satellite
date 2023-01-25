f4452eb4025bc185bd8d3f4694609fb7372d5988
LegendOfGIT <marcel.schneider1@googlemail.com>
2023-01-25 11:25:38 +0100

    Update visit-uri.js

--- src/scraping/commands/visit-uri.js
+++ src/scraping/commands/visit-uri.js
@@ -36,9 +36,7 @@ module.exports = (context = {}, parameters = {}) => new Promise(resolve => {
 
     const options = {
         headers: {
-            "User-Agent": generateString(8),
-            "Accept-Language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+            "User-Agent": generateString(8)
         }
     };
     
