7dc34b8b8e36f768f58f329bc187d4f94dc54fe3
LegendOfGIT <marcel.schneider1@googlemail.com>
2023-01-25 11:27:20 +0100

    Update visit-uri.js

--- src/scraping/commands/visit-uri.js
+++ src/scraping/commands/visit-uri.js
@@ -3,7 +3,7 @@ const getPreparedCommandParameters = require('../getPreparedCommandParameters');
 
 const generateString = (length) => {
     const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = ' ';
+    let result = '';
     const charactersLength = characters.length;
     for ( let i = 0; i < length; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
