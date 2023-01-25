4218d5c480e89e002d8892cc2122052593119811
mmarschner <marcel.marschner@adesso.de>
2023-01-25 13:56:01 +0100

    - visit-uri no longer uses curl requests

--- src/scraping/commands/visit-uri.js
+++ src/scraping/commands/visit-uri.js
@@ -1,4 +1,4 @@
-const { curly } = require('node-libcurl');
+const request = require('request');
 const getPreparedCommandParameters = require('../getPreparedCommandParameters');
 
 const generateString = (length) => {
@@ -34,20 +34,29 @@ module.exports = (context = {}, parameters = {}) => new Promise(resolve => {
         { uri: uri || '' }
     ));
 
-    curly.get(commandParameters.uri, {
-      httpHeader: [
-        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-      ]
-    })
-    .then(body => {
-        if (contextId) {
-            context[contextId] = body;
-        }
-        resolve();
-    })
-    .catch(e => {
-        console.log(`requested uri "${commandParameters.uri}" can not be resolved. abort`);
-        resolve();
-    });
-});
+    request(
+        {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+            },
+            uri: commandParameters.uri,
+            method: 'GET'
+        },
+        function (err, res, body) {
+            console.log(err);
+            console.log(res);
 
+            if (err) {
+                console.log(`requested uri "${commandParameters.uri}" can not be resolved. abort`);
+                resolve();
+                return;
+            }
+
+            if (contextId) {
+                context[contextId] = body;
+            }
+
+            resolve();
+        }
+    );
+});
