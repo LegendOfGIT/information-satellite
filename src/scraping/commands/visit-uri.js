687f89c57455fcaf7cbf99915e88f1771d1ce70c
mmarschner <marcel.marschner@adesso.de>
2023-01-25 16:19:04 +0100

    - visit-uri no longer uses curl requests - adjusted search url for amazon switch games

--- src/scraping/commands/visit-uri.js
+++ src/scraping/commands/visit-uri.js
@@ -24,8 +24,8 @@ module.exports = (context = {}, parameters = {}) => new Promise(resolve => {
     ));
 
     const options = {
-        "headers": {
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
         }
     };
     return httpClient.get(commandParameters.uri, options)
