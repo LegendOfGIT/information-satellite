63b4c9d462a9490abaa0d0fa318a83f0181368e5
LegendOfGIT <marcel.schneider1@googlemail.com>
2023-01-25 12:56:22 +0100

    Update visit-uri.js

--- src/scraping/commands/visit-uri.js
+++ src/scraping/commands/visit-uri.js
@@ -37,7 +37,7 @@ module.exports = (context = {}, parameters = {}) => new Promise(resolve => {
     request(
         {
             headers: {
-                'User-Agent': generateString(8)
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
             },
             uri: commandParameters.uri,
             method: 'GET'
