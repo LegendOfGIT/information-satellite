ead066c1c236827ea571d50978d70414c5f39839
mmarschner <marcel.marschner@adesso.de>
2023-01-25 14:49:19 +0100

    - visit-uri now uses curl requests

--- src/scraping/commands/visit-uri.js
+++ src/scraping/commands/visit-uri.js
@@ -38,7 +38,7 @@ module.exports = (context = {}, parameters = {}) => new Promise(resolve => {
     var args = ' -H "User-Agent: '+ generateString(8) +'" ' + commandParameters.uri;
 
     exec('curl ' + args, function (error, stdout, stderr) {
-      console.log(error);
+      console.log(stdout.indexOf('s-asin'));
 
       if (error !== null) {
         resolve();
