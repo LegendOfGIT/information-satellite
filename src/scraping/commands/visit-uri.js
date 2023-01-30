1e8321ee48b9984c0195d7445d2e506651c8f6ef
Marcel Marschner <marcel.marschner@outlook.com>
2023-01-30 22:52:58 +0100

    - added usage of proxy for visit-uri

--- src/scraping/commands/visit-uri.js
+++ src/scraping/commands/visit-uri.js
@@ -32,11 +32,18 @@ module.exports = (context = {}, parameters = {}) => new Promise(resolve => {
         return;
     }
 
+    const proxyHosts = [ '149.129.239.170', '132.129.121.148', '154.129.98.156', '211.129.132.150', '164.129.114.111' ];
     const options = {
         headers: {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+            proxy: {
+                protocol: 'http',
+                host: proxyHosts[Math.floor(Math.random() * proxyHosts.length)],
+                port: 8080
+            },
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
         }
     };
+
     return httpClient.get(commandParameters.uri, options)
         .then(response => {
             console.log(`requested uri "${commandParameters.uri}" was resolved successfully.`);
