0bdc4dafdd77e77edb1aa55cff0fa37b428f85a1
LegendOfGIT <marcel.schneider1@googlemail.com>
2023-03-20 00:53:30 +0100

    Update visit-uri.js
    -

--- src/scraping/commands/visit-uri.js
+++ src/scraping/commands/visit-uri.js
@@ -35,9 +35,9 @@ module.exports = (context = {}, parameters = {}) => new Promise(resolve => {
 
     const options = {
         headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-            httpsAgent: new https.Agent({ rejectUnauthorized: false })
-        }
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        },
+        httpsAgent: new https.Agent({ rejectUnauthorized: false })
     };
 
     return httpClient.get(commandParameters.uri, options)
