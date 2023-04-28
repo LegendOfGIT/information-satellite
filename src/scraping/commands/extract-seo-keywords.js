28caf5eb280244bb3434108ac7b84d298e271bf6
Marcel Marschner <marcel.marschner@dfb.de>
2023-04-28 09:29:58 +0200

    - introduced automatic seo keyword generation

--- src/scraping/commands/extract-seo-keywords.js
+++ src/scraping/commands/extract-seo-keywords.js
@@ -52,7 +52,7 @@ module.exports = (context = {}, parameters = {}) => new Promise(resolve => {
     });
 
     terms = terms
-        .replaceAll(/(\.|:|,|!|;)/g, '')
+        .replaceAll(/(\.|:|,|!|;|')/g, '')
         .replaceAll('-', ' ');
 
     const termCountMap = {};
