35b35394e661550ed1e1f35e04ae67b06dec3355
Marcel Marschner <marcel.marschner@dfb.de>
2023-08-23 11:34:38 +0200

    - added categories - added shop "saunaloft"

--- src/scraping/commands/clone-items-and-modify-clones.js
+++ src/scraping/commands/clone-items-and-modify-clones.js
@@ -1,7 +1,7 @@
 module.exports = (context = {}, parameters = {}) => new Promise(resolve => {
     console.log('executing command "clone-items-and-modify-clones"')
 
-    const { contextId, sourceContextId, replacements = {}, unique = false } = parameters;
+    const { contextId, sourceContextId, regex = undefined, replacements = {}, unique = false } = parameters;
 
     if (!contextId) {
         console.log('required parameter "contextId" is not given. abort.');
@@ -23,11 +23,20 @@ module.exports = (context = {}, parameters = {}) => new Promise(resolve => {
     let items = [];
     sourceContext.forEach(item => {
         let clonedItem = item;
+        if (regex) {
+            const matches = clonedItem.matchAll(new RegExp(regex, "g"));
+            const matchingValues = [];
+            for (let match of matches) {
+                matchingValues.push(match.length > 1 ? match[1] : match[0]);
+            }
+            clonedItem = matchingValues && matchingValues.length ? matchingValues[0] : clonedItem;
+        }
+
         for (const [key, value] of Object.entries(replacements)) {
             clonedItem = clonedItem.replace(new RegExp(key,"g"), value);
         }
 
-        items.push(clonedItem);
+        items.push((clonedItem || '').trim());
     });
 
     context[contextId] = unique
