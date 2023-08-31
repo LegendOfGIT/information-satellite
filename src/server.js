740763a6a076372f77f25efbbe859aabf655eebb
Marcel Marschner <marcel.marschner@dfb.de>
2023-08-31 22:47:35 +0200

    - added new categories

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
+const observe = require("./observe");
 const configuration = require('./configuration/app-config')();
+const observeConfiguration = require('./configuration/observe-config');
 const fastify = require('fastify')({
     logger: true
+});
+
+fastify.get('/observe/configuration', async (request, reply) => {
+    reply.type('application/json').code(200);
+    reply.send(observeConfiguration);
 });
 
 fastify.get('/observe/site/:site/use-case/:useCase', async (request, reply) => {
     reply.type('application/json').code(200);
 
-    const observe = require('./observe');
     reply.send(observe(
         request.params.site,
         request.params.useCase,
@@ -14,7 +20,7 @@ fastify.get('/observe/site/:site/use-case/:useCase', async (request, reply) => {
         request.query.itemCanonical,
         (request.query.navigationPath || '').split(',')
     ));
-})
+});
 
 fastify.listen({ host: configuration.application.host, port: 3000 }, (err, address) => {
     if (err) throw err
