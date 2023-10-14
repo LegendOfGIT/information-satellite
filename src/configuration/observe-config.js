bfc464b6b2e63c2f85cd86e1eb464a811e9f720e
Marcel Marschner <marcel.marschner@dfb.de>
2023-10-14 23:24:48 +0200

    - added new category

--- src/configuration/observe-config.js
+++ src/configuration/observe-config.js
@@ -129,6 +129,7 @@ module.exports = {
     HOME_WELLNESS_SAUNAS: 'saunas',
     HOME_WELLNESS_WHIRLPOOLS: 'whirlpools',
     KIDS_BOOKS_AUDIOBOOKS: 'kids-audio-books',
+    KIDS_BOOKS_READINGBOOKS: 'kids-reading-books',
     KIDS_PARTY_TABLEWARE: 'kids-party-tableware',
     KIDS_SCHOOL_DRINKINGBOTTLES: 'kids-school-drinking-bottles',
     KIDS_SCHOOL_LUNCHBOXES: 'kids-school-lunchboxes',
